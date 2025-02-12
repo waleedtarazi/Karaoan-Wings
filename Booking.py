fb58c93b56b6b87408035aa1c83760efee7ad2ed
waleedtarazi <waleedaltarazy@gmail.com>
2025-02-12 20:14:39 +0300

    yeah new line

---
+++ Booking.py
@@ -0,0 +1,11 @@
+import random
+from .Flight import Flight
+class Booking():
+    def __init__(self, flight, type, status, user):
+        self.id = random.random()
+        self.flight = flight
+        self.type = type
+        self.status = status
+        self.user = user
+# new line 
+        
