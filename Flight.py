57b52c0a9ab39801bb0fbb64a325d49027c51a6e
waleedtarazi <waleedaltarazy@gmail.com>
2025-02-12 19:40:16 +0300

    modify/Flight class, add number of seats

--- Flight.py
+++ Flight.py
@@ -1,7 +1,8 @@
 import random
 
 class Flight():
-    def __init__(self, departure, destination):
+    def __init__(self, departure, destination, number_of_seats):
         self.departure = departure
         self.destination = destination
+        self.number_of_seats = number_of_seats
         self.id = random.random()
